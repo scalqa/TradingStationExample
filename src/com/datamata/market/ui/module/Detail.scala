cd8f4af09829a76eda7b2f5578e1aaec77c98145
edolgy <edolgy@gmail.com>
2021-10-11 12:02:33 -0400

    .

--- src/com/datamata/market/ui/module/Detail.scala
+++ src/com/datamata/market/ui/module/Detail.scala
@@ -1,6 +1,6 @@
 package com.datamata; package market; package ui; package module; import language.implicitConversions
 
-abstract class Detail[A<:AnyRef] extends Module with Pro.OM.X.Basis[A]:
+abstract class Detail[A<:AnyRef] extends Module with Pro.OM.X.Base[A]:
   def this(v: A) = { this(); update(v) }
 
   def bindTo(v: Pro.O[_ <: A])                                : Event.Control = v.onChange(() => this() = v())
